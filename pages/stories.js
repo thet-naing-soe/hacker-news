0290f1f18256409865638baadf30aa883fb1de28
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-11-11 11:55:25 +0630

    first commit

---
+++ pages/stories.js
@@ -0,0 +1,31 @@
+import Story from '../components/Story.js';
+import view from '../utils/view.js';
+
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  const hasStories = stories.length > 0;
+                    
+  view.innerHTML = `<div>
+    ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1 })).join('') : 'No stories'}
+  </div>`;  
+}
+
+async function getStories(path) {
+  const isHomeRoute = path === '/';
+  const isNewRoute = path === '/new';
+  if (isHomeRoute) {
+    path = '/news';  
+  } else if (isNewRoute) {
+    path = '/newest';  
+  } 
+  const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+  const stories = await response.json();
+  return stories;
+}
+
+// https://node-hnapi.herokuapp.com
+
+// / (Top) -> /news
+// /new (New) -> /newest
+// /ask (Ask) -> /ask
+// /show (Show) -> /show 
